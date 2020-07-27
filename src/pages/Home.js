665ac1bb935f9d9f85da420c80913bdab505b5c7
dmytro.kukharuk <Di80977298968>
2020-07-27 13:34:19 +0300

    created a FireBase project, state, reducer, context and configured my project depending on them

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useContext, useEffect} from "react";
 import {Forme} from "../component/Forme";
 import {Notes} from "../component/Notes/Notes";
+import {FirebaseContext} from "../context/firebase/firebaseContext";
 
 export const Home = () => {
-  const notes = new Array(3)
-      .fill('')
-      .map((_, i) => ({id:i, title: `Note ${i + 1}`}))
+  const {loading, notes, fetchNotes} =useContext(FirebaseContext)
+
+  useEffect(() => {
+    fetchNotes()
+    // eslint-disable-next-line
+  }, [])
+
   return (
     <Fragment>
       <Forme />
