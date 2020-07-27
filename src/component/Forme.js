900ae33c6ecf5622d128db22ad111ab13afeec2e
dmytro.kukharuk <Di80977298968>
2020-07-27 10:50:55 +0300

    embedded a notification into the form

--- src/component/Forme.js
+++ src/component/Forme.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import {AlertContext} from "../context/Alert/alertContext";
 
 export const Forme = () => {
+  const [value, setValue] = useState('')
+  const alert = useContext(AlertContext)
+
+  const submitHandler = event => {
+    event.preventDefault()
+
+    if (value.trim()) {
+      // ...
+      alert.show(' Note was created', 'success')
+      setValue('')
+    } else {
+      alert.show(' Enter note name')
+    }
+  }
 
   return (
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="form-group">
           <input
               type="text"
               className="form-control"
               placeholder="Enter notes name"
+              value={value}
+              onChange={e => setValue(e.target.value)}
           />
         </div>
       </form>
